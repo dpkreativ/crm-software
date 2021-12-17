62627c7361c2fc3dad916f46c457533bf34c1043
Divine Orji <dpkreativ@gmail.com>
2021-12-17 05:01:33 +0100

    got started on the landing page

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>Hello Divi</main>
+      <main>
+        <section>
+          <h1>CRM Software</h1>
+          <p>A simple demo of a customer relationship management software</p>
+        </section>
+      </main>
     </div>
   );
 }
