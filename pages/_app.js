95b5b63acbefdb07ba69dd1a3a2e0fc62168a9c8
Divine Orji <dpkreativ@gmail.com>
2021-12-17 05:45:57 +0100

    build out landing page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="max-w-screen-lg mx-auto p-5 lg:p-10">
+      <Component {...pageProps} />
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -10,11 +12,38 @@ export default function Home() {
       </Head>
 
       <main>
-        <section>
-          <h1>CRM Software</h1>
+        <section className="text-center">
+          <div className="h-60 lg:h-72 relative">
+            <Image src="/customer.svg" layout="fill" />
+          </div>
+          <h1 className="font-bold text-4xl lg:text-6xl mb-5">CRM Software</h1>
           <p>A simple demo of a customer relationship management software</p>
         </section>
+        <section className="text-center my-10">
+          <Link href="/" passHref>
+            <a>
+              <button className="bg-primary-100 px-5 py-3 rounded-2xl text-white font-semibold hover:bg-primary-200 hover:shadow">
+                View Demo
+              </button>
+            </a>
+          </Link>
+        </section>
       </main>
+
+      <footer className="text-center">
+        Built using
+        <Link href="https://nextjs.org/" passHref>
+          <a className="font-semibold cursor-pointer"> Next.js</a>
+        </Link>
+        ,
+        <Link href="https://tailwindcss.com/" passHref>
+          <a className="font-semibold cursor-pointer"> TailwindCSS</a>
+        </Link>
+        , and
+        <Link href="https://fauna.com/" passHref>
+          <a className="font-semibold cursor-pointer"> FaunaDB</a>
+        </Link>
+      </footer>
     </div>
   );
 }
