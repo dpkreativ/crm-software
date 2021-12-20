73f9b67689c71f95fd4f59f9a9c63bf1c41f23f3
Divine <dpkeativ@gmail.com>
2021-12-20 15:40:21 +0100

    build sidebar tab switch

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,33 +1,71 @@
+import { useState } from "react";
 import { Calendar, HeartOutline, Note, Trash, UserCircle } from "./Icons";
 
 const Sidebar = () => {
+  const [isActive, setIsActive] = useState(1);
+
   return (
-    <div className="h-screen w-1/5 p-10 flex flex-col space-y-10">
-      <div className="text-primary-100 flex space-x-3">
+    <div className="h-screen w-1/5 p-10 flex flex-col space-y-7">
+      <div
+        className={`${
+          isActive === 1
+            ? "shadow-md bg-primary-100 text-white font-semibold"
+            : ""
+        } flex space-x-3 p-2 rounded-xl hover:cursor-pointer hover:shadow`}
+        onClick={() => setIsActive(1)}
+      >
         <div>
           <UserCircle />
         </div>
         <div>Connections</div>
       </div>
-      <div className="text-primary-100 flex space-x-3">
+      <div
+        className={`${
+          isActive === 2
+            ? "shadow-md bg-primary-100 text-white font-semibold"
+            : ""
+        } flex space-x-3 p-2 rounded-xl hover:cursor-pointer hover:shadow`}
+        onClick={() => setIsActive(2)}
+      >
         <div>
           <HeartOutline />
         </div>
         <div>Favourites</div>
       </div>
-      <div className="text-primary-100 flex space-x-3">
+      <div
+        className={`${
+          isActive === 3
+            ? "shadow-md bg-primary-100 text-white font-semibold"
+            : ""
+        } flex space-x-3 p-2 rounded-xl hover:cursor-pointer hover:shadow`}
+        onClick={() => setIsActive(3)}
+      >
         <div>
           <Calendar />
         </div>
         <div>Calendar</div>
       </div>
-      <div className="text-primary-100 flex space-x-3">
+      <div
+        className={`${
+          isActive === 4
+            ? "shadow-md bg-primary-100 text-white font-semibold"
+            : ""
+        } flex space-x-3 p-2 rounded-xl hover:cursor-pointer hover:shadow`}
+        onClick={() => setIsActive(4)}
+      >
         <div>
           <Note />
         </div>
         <div>Notes</div>
       </div>
-      <div className="text-primary-100 flex space-x-3">
+      <div
+        className={`${
+          isActive === 5
+            ? "shadow-md bg-primary-100 text-white font-semibold"
+            : ""
+        } flex space-x-3 p-2 rounded-xl hover:cursor-pointer hover:shadow`}
+        onClick={() => setIsActive(5)}
+      >
         <div>
           <Trash />
         </div>
