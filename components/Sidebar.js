a59ac40f13ed774b7932f892f9f09b2cb947d2e7
Divine <dpkeativ@gmail.com>
2021-12-20 13:48:03 +0100

    create sidebar component

---
+++ components/Sidebar.js
@@ -0,0 +1,23 @@
+const Sidebar = () => {
+  return (
+    <div className="h-screen w-1/5 bg-gray-200">
+      <section>
+        <div>Connections</div>
+      </section>
+      <section>
+        <div>Favourites</div>
+      </section>
+      <section>
+        <div>Calendar</div>
+      </section>
+      <section>
+        <div>Notes</div>
+      </section>
+      <section>
+        <div>Trash</div>
+      </section>
+    </div>
+  );
+};
+
+export default Sidebar;
