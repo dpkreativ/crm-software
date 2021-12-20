c562c7ecb56b563bb93d11896f38c2554d9daf07
Divine <dpkeativ@gmail.com>
2021-12-20 15:17:36 +0100

    insert icons to sidebar

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -1,11 +1,38 @@
+import { Calendar, HeartOutline, Note, Trash, UserCircle } from "./Icons";
+
 const Sidebar = () => {
   return (
-    <div className="h-screen w-1/5 bg-gray-200 p-10 flex flex-col space-y-10">
-      <div>Connections</div>
-      <div>Favourites</div>
-      <div>Calendar</div>
-      <div>Notes</div>
-      <div>Trash</div>
+    <div className="h-screen w-1/5 p-10 flex flex-col space-y-10">
+      <div className="text-primary-100 flex space-x-3">
+        <div>
+          <UserCircle />
+        </div>
+        <div>Connections</div>
+      </div>
+      <div className="text-primary-100 flex space-x-3">
+        <div>
+          <HeartOutline />
+        </div>
+        <div>Favourites</div>
+      </div>
+      <div className="text-primary-100 flex space-x-3">
+        <div>
+          <Calendar />
+        </div>
+        <div>Calendar</div>
+      </div>
+      <div className="text-primary-100 flex space-x-3">
+        <div>
+          <Note />
+        </div>
+        <div>Notes</div>
+      </div>
+      <div className="text-primary-100 flex space-x-3">
+        <div>
+          <Trash />
+        </div>
+        <div>Trash</div>
+      </div>
     </div>
   );
 };
