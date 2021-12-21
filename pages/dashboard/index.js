ec589d8e608e9bf6db3f40efd2d334c3b241c2c4
Divine <dpkeativ@gmail.com>
2021-12-21 09:02:55 +0100

    add search bar and new contact button

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -10,8 +10,26 @@ const Dashboard = () => {
       {/* Content */}
       <section>
         <div className="p-5">
-          Note: This is a demo. Do not store any real numbers on this app as it
-          will be visible to everyone else. Thanks.
+          <div className="text-xs mb-2">
+            Note: This is a demo. Do not store any real numbers on this app as
+            it will be visible to everyone else. Thanks.
+          </div>
+          <div className="flex items-center space-x-5">
+            <div className="p-2 border-2 border-primary-100 rounded-2xl w-96 hover:border-primary-200 hover:shadow">
+              <input
+                type="search"
+                name="searchContact"
+                id="searchContact"
+                placeholder="Find connection"
+                className="outline-none w-full"
+              />
+            </div>
+            <div>
+              <button className="p-2 bg-primary-100 rounded-2xl text-white shadow-lg hover:bg-primary-200 hover:shadow">
+                New Connection
+              </button>
+            </div>
+          </div>
         </div>
         <div className="p-5">
           <ContactCard />
