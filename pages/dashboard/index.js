11dd9982f8b798c14f89bd7eea3d7d3e1b64a1ab
Divine <dpkeativ@gmail.com>
2021-12-21 12:18:02 +0100

    update classes

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -4,13 +4,13 @@ import Sidebar from "components/Sidebar";
 
 const Dashboard = () => {
   return (
-    <div className="flex">
+    <div className="flex h-screen">
       {/* Sidebar */}
       <Sidebar active={1} />
 
       {/* Content */}
-      <section>
-        <div className="p-5">
+      <section className="w-4/5 relative h-full bg-red-200">
+        <div className="p-5 sticky top-0 bg-white">
           <div className="text-xs mb-2">
             Note: This is a demo. Do not store any real numbers on this app as
             it will be visible to everyone else. Thanks.
@@ -22,7 +22,7 @@ const Dashboard = () => {
                 name="searchContact"
                 id="searchContact"
                 placeholder="Find connection"
-                className="outline-none w-full"
+                className="outline-none w-full bg-transparent"
               />
             </div>
             <div>
@@ -33,8 +33,11 @@ const Dashboard = () => {
             </div>
           </div>
         </div>
-        <div className="p-5">
-          <ContactCard />
+        <div className="flex w-full h-full">
+          <div className="p-5 w-1/3 bg-gray-100">
+            <ContactCard />
+          </div>
+          <div className="p-5 w-2/3 bg-gray-300"></div>
         </div>
       </section>
     </div>
