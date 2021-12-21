0130f327ac3725f8d0e1f54c49da654f14ee6596
Divine <dpkeativ@gmail.com>
2021-12-21 08:34:24 +0100

    get started on dashboard

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -3,8 +3,17 @@ import Sidebar from "components/Sidebar";
 const Dashboard = () => {
   return (
     <div className="flex">
+      {/* Sidebar */}
       <Sidebar active={1} />
-      <div>Hello, I'm the Dashboard.</div>
+
+      {/* Content */}
+      <section>
+        <div>
+          Note: This is a demo. Do not store any real numbers on this app as it
+          will be visible to everyone else. Thanks.
+        </div>
+        <div>All available contacts</div>
+      </section>
     </div>
   );
 };
