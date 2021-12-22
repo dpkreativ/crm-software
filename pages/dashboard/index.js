f470f77a1e1a59b6af3d3083853d5826a819b248
Divine <dpkeativ@gmail.com>
2021-12-22 08:44:39 +0100

    style and content updates

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -4,19 +4,28 @@ import Sidebar from "components/Sidebar";
 
 const Dashboard = () => {
   return (
-    <div className="flex h-screen">
+    <div className="flex h-screen overflow-hidden">
       {/* Sidebar */}
       <Sidebar active={1} />
 
       {/* Content */}
-      <section className="w-4/5 relative h-full bg-red-200">
-        <div className="p-5 sticky top-0 bg-white">
-          <div className="text-xs mb-2">
-            Note: This is a demo. Do not store any real numbers on this app as
-            it will be visible to everyone else. Thanks.
-          </div>
-          <div className="flex items-center space-x-5">
-            <div className="p-2 border-2 border-primary-100 rounded-xl w-96 hover:border-primary-200 hover:shadow">
+      <section className="w-4/5 relative flex">
+        <div className="h-screen w-1/3">
+          <div className="p-5 sticky top-0 bg-white h-1/5">
+            <div className="flex items-center mb-2 space-x-2">
+              <div className="text-xs">
+                Note: This is a demo. Any numbers stored on this app will be
+                visible to everyone else.
+              </div>
+              <div>
+                <button className="p-2 bg-primary-100 rounded-xl text-white shadow-lg hover:bg-primary-200 hover:shadow flex space-x-2">
+                  <AddContact />
+                  <div className="font-semibold">New</div>
+                </button>
+              </div>
+            </div>
+
+            <div className="p-2 border-2 border-primary-100 rounded-xl hover:border-primary-200 hover:shadow">
               <input
                 type="search"
                 name="searchContact"
@@ -25,20 +34,25 @@ const Dashboard = () => {
                 className="outline-none w-full bg-transparent"
               />
             </div>
-            <div>
-              <button className="p-2 bg-primary-100 rounded-xl text-white shadow-lg hover:bg-primary-200 hover:shadow flex space-x-2">
-                <AddContact />
-                <div className="font-semibold">New Connection</div>
-              </button>
-            </div>
           </div>
-        </div>
-        <div className="flex w-full h-full">
-          <div className="p-5 w-1/3 bg-gray-100">
+          <div className="p-5 h-4/5 overflow-y-scroll">
+            <ContactCard />
+            <ContactCard />
+            <ContactCard />
+            <ContactCard />
+            <ContactCard />
+            <ContactCard />
+            <ContactCard />
+            <ContactCard />
+            <ContactCard />
+            <ContactCard />
+            <ContactCard />
+            <ContactCard />
             <ContactCard />
           </div>
-          <div className="p-5 w-2/3 bg-gray-300"></div>
         </div>
+
+        <div className="h-screen w-2/3 p-5 overflow-y-scroll"></div>
       </section>
     </div>
   );
