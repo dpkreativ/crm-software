64971b8732eb2c305d78f11ec7c2b0e1a95f82d7
Divine Orji <dpkreativ@gmail.com>
2021-12-31 09:15:42 +0100

    create note message component

---
+++ components/atoms/NoteMessage.js
@@ -0,0 +1,10 @@
+const NoteMessage = () => {
+  return (
+    <div className="text-xs">
+      Note: This is a demo. Any numbers stored on this app will be visible to
+      everyone else.
+    </div>
+  );
+};
+
+export default NoteMessage;
