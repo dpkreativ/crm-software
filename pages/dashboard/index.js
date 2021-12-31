498320e73ce46e0f5590a5b44dfdfa62870cd89c
Divine Orji <dpkreativ@gmail.com>
2021-12-31 09:21:05 +0100

    update dashboard landing page component

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,63 +1,44 @@
-import ContactCard from "components/ContactCard";
+import DashboardTemplate from "@/templates/DashboardTemplate";
 import ContactDetails from "components/ContactDetails";
-import { AddContact } from "components/Icons";
-import Sidebar from "components/Sidebar";
+import { AddContact } from "@/atoms/Icons";
+import Search from "@/atoms/Search";
+import Button from "@/atoms/Button";
+import NoteMessage from "@/atoms/NoteMessage";
+import ContactList from "@/organisms/ContactList";
 
 const Dashboard = () => {
   return (
-    <div className="flex h-screen overflow-hidden">
-      {/* Sidebar */}
-      <Sidebar active={1} />
-
-      {/* Content */}
-      <section className="w-4/5 relative flex">
+    <DashboardTemplate sidebarActiveTab={1}>
+      <div className="w-full flex">
+        {/* Contact List Section */}
         <div className="h-screen w-1/3 overflow-y-scroll">
+          {/* Top */}
           <div className="p-5 sticky top-0 z-10 shadow-sm bg-white">
             <div className="flex items-center space-x-2">
-              <div className="text-xs">
-                Note: This is a demo. Any numbers stored on this app will be
-                visible to everyone else.
-              </div>
+              <NoteMessage />
               <div>
-                <button className="p-2 bg-primary-100 rounded-xl text-white shadow-lg hover:bg-primary-200 hover:shadow flex space-x-2">
-                  <AddContact />
-                  <div className="font-semibold">New</div>
-                </button>
+                <Button icon={<AddContact />} text="New" />
               </div>
             </div>
           </div>
+          {/* Bottom */}
+          <div className="py-5">
+            <Search
+              searchName="searchContact"
+              searchId="searchContact"
+              placeholder="Find connection"
+            />
 
-          <div>
-            <div className="mx-5 p-2 border-2 border-primary-100 rounded-xl hover:border-primary-200 hover:shadow">
-              <input
-                type="search"
-                name="searchContact"
-                id="searchContact"
-                placeholder="Find connection"
-                className="outline-none w-full bg-transparent"
-              />
-            </div>
-            <ContactCard favourite={true} />
-            <ContactCard />
-            <ContactCard />
-            <ContactCard />
-            <ContactCard />
-            <ContactCard />
-            <ContactCard />
-            <ContactCard />
-            <ContactCard />
-            <ContactCard />
-            <ContactCard />
-            <ContactCard />
-            <ContactCard />
+            <ContactList />
           </div>
         </div>
 
+        {/* Contact Details Section */}
         <div className="h-screen w-2/3 overflow-y-scroll">
           <ContactDetails />
         </div>
-      </section>
-    </div>
+      </div>
+    </DashboardTemplate>
   );
 };
 
